ca6e7f9e132e6e6a67488a8cd93ad59d6ff99eb4
Micaela Ybarra <mica.yaz03@gmail.com>
2026-02-02 17:31:04 -0300

    protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,30 @@ import Dashboard from "../src/pages/Dashboard"
 import Appointments from "../src/pages/Appointments";
 import Navbar from "../src/components/Navbar";
 
+import ProtectedRoute from "./components/ProtectedRoute"
+import PrivateLayout from "./components/PrivateLayout"
+
 function App() {
   return (
     <Router>
       <Navbar/>
       <Routes>
+
+        { /* Ruta pública */ }
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/appointments" element={<Appointments />} />
+
+        { /* Rutas privadas */ }
+
+        <Route element={
+          <ProtectedRoute>
+              <PrivateLayout/>
+          </ProtectedRoute> } >
+
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/appointments" element={<Appointments />} />
+
+        </Route>
+
       </Routes>
     </Router>
   );
