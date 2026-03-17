5cb1ef093db313c82f6bcf9cb444760ea7a37b8f
Micaela Ybarra <mica.yaz03@gmail.com>
2026-03-16 22:36:45 -0300

    result is not defined fixed

--- src/components/PopUpBlock.jsx
+++ src/components/PopUpBlock.jsx
@@ -26,8 +26,8 @@ export default function PopUpBlock({ availabilityBlockId }) {
       setBlock(res.data.data || []);
 
       setFormData({
-        available: result.data.available,
-        reason: result.data.reason || "",
+        available: res.data.data.available,
+        reason: res.data.data.reason || "",
       });
 
     } catch (err) {
@@ -47,7 +47,6 @@ export default function PopUpBlock({ availabilityBlockId }) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const token = localStorage.getItem("token");
 
     await api.patch(`/appointment/block/${availabilityBlockId}`, formData);
 
